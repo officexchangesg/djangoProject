2e2b0af0c4f7533fe8b8fdac391d9b2587fef80f
officexchangesg <officexchangesg@gmail.com>
2022-10-03 10:24:44 +0800

    ADD Post model

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post
 
+#admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "body"]
+    list_filter = ["status", "author"]
+    search_fields = ["title","body"]
+    prepopulated_fields = {"slug": ("title","author")}
+    raw_id_fields = ["author"]
+    date_hierarchy = "publish"
+    ordering = ["status","publish"]
 # Register your models here.
