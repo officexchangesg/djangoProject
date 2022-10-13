77872297a3e4776261b2808bb156a7fb00524ed2
officexchangesg <officexchangesg@gmail.com>
2022-10-13 14:17:08 +0800

    update Post model

--- images/views.py
+++ images/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
+from django.shortcuts import get_object_or_404
+from .models import Image
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+                  'images/image/detail.html',
+                  {'section': 'images',
+                   'image': image})
 @login_required
 def image_create(request):
     if request.method == 'POST':
